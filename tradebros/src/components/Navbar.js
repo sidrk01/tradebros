38dcd1243fa2d40ea7e221e9515a0534ab5e1985
Blake Dickerson <blakedickerson@Blakes-MBP.lan>
2023-01-26 18:46:50 -0800

    Fixing package not found error

--- tradebros/src/components/Navbar.js
+++ tradebros/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Button from "./Button";
 import './navbar.css'
 
 export default function Navbar() {
