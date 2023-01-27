1a74b8802aacfd423c4091bf3ee454a154e6fed3
Blake Dickerson <blakedickerson@Blakes-MBP.lan>
2023-01-26 23:32:04 -0800

    Finished rough navbar, added a dropdown menu for the features

---
+++ tradebros/src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Virtual Portfolio',
+        path: '/virtual_portfolio',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'News',
+        path: '/news',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Watchlists',
+        path: '/watchlists',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Stock Performance',
+        path: '/stock_performance',
+        cName: 'dropdown-link'
+    }
+];
--- tradebros/src/components/Navbar.js
+++ tradebros/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './navbar.css'
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Dropdown from "./Dropdown";
 
 export default function Navbar() {
 
@@ -8,42 +10,65 @@ export default function Navbar() {
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
+    const [dropdown, setDropdown] = useState(false);
+    const onMouseEnter = () => {
+        if(window.innerWidth < 960) {
+            setDropdown(false);
+        }
+        else {
+            setDropdown(true);
+        }
+    }
+    const onMouseLeave =() => {
+        if(window.innerWidth < 960) {
+            setDropdown(false);
+        }
+        else {
+            setDropdown(false);
+        }
+    }
+
+
   return (
     <header>
+        {/* This will hold the logo link */}
         <Link to="/" className="logo"><i class='bx bx-happy'></i>
             TradeBros.
         </Link>
-        <div className="menu-icon" onClick={handleClick}>
-            {/* <i className="fas fa-bars" /> */}
-        </div>
 
+        {/* The menu icon element */}
+        <div className="menu-icon" onClick={handleClick}></div>
+
+        {/* List to hold all of the navbar elements */}
         <ul className="navbar">
+            {/* Begin navbar item 1 */}
             <li>
                 <Link to="/learn" className="navbar-links" onClick={closeMobileMenu}>
                     Learn
                 </Link>
             </li>
-            <li>
+            {/* Begin navbar item 2 */}
+            <li onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
                 <Link to="/features" className="navbar-links" onClick={closeMobileMenu}>
                     Features
+                    <ExpandMoreIcon className="dropdown-button"/>
                 </Link>
                 {/* Make dropdown menu here */}
+                {dropdown && <Dropdown/>}
             </li>
+            {/* Begin navbar item 3 */}
             <li>
                 <Link to="/market" className="navbar-links" onClick={closeMobileMenu}>
                     Market
                 </Link>
             </li>
-            <li>
-                <Link to="/news" className="navbar-links" onClick={closeMobileMenu}>
-                    News
-                </Link>
-            </li>
+            {/* Begin navbar item 4 */}
             <li>
                 <Link to="/pricing" className="navbar-links" onClick={closeMobileMenu}>
                     Pricing
                 </Link>
             </li>
+            {/* Begin navbar item 5 */}
             <li>
                 <Link to="/support" className="navbar-links" onClick={closeMobileMenu}>
                     Support
@@ -51,7 +76,7 @@ export default function Navbar() {
             </li>
         </ul>
 
-
+        {/* Div container to hold the sign in and login buttons */}
         <div className="header-btn">
             <Link to="/sign-up" className="sign-up">
                 Sign Up
