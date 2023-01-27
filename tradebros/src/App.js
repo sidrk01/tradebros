f24cc48c73dd0e1fb16390ac2dd0a5374d346a34
skiislam <sisla016@ucr.edu>
2023-01-26 18:20:35 -0800

    Created the three cards for the Learn page

--- tradebros/src/App.js
+++ tradebros/src/App.js
@@ -1,11 +1,13 @@
 // import './App.css';
 import React, { Component }  from 'react';
+import Learn from './Pages/Learn';
 
 function App() {
   return (
     <div className="App">
-      hi
-      gilfs
+      <Learn>
+        
+      </Learn>
     </div>
   );
 }
---
+++ tradebros/src/Pages/Learn.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Learn.css'
+export default function Learn() {
+    return (
+       <div className='container'>
+        {/* <h3> I LIKE BIG BUTTS </h3> */}
+        <div className='box'>
+            <h3 className='top1'> what is an</h3>
+            <h3> investment?</h3>
+            <p className='Sentence'> An investment is an asset bought with the expectation that it will generate some future income or profit</p>
+        </div>
+        <div className='box'>
+        <h3 className='top1'> what is a</h3>
+            <h3> stock?</h3>
+            <p className='Sentence'> A stock is a unit of ownership in a company.</p>
+            
+        </div>
+        <div className='box'>
+        <h3 className='top1'> what is the</h3>
+            <h3> stock market?</h3>
+            <p className='Sentence'> The stock market is where buyers and sellers come together to trade shares in eligible companies</p>
+            
+        </div>
+       </div>
+
+
+    
+    )
+}
+
