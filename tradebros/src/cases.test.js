1e3a420e5d4b664753ee21576270b767959935db
mallie2000 <muzammilallie230@gmail.com>
2023-02-21 18:06:20 -0800

    Added test cases for testing

--- tradebros/src/cases.test.js
+++ tradebros/src/cases.test.js
@@ -1,4 +1,37 @@
-test('adds 1 + 2 to equal 3', () => {
-    let testVal = 3;
-    expect(testVal == 3);
+test("test_full_api_link", () => {
+  const key = "bv1uf4v48v6o5ed6h88g";
+  const BASE_URL = "https://finnhub.io/api/v1/quote?symbol=";
+  const KEY_URL = '&token=${key}';
+  const final_api = BASE_URL + KEY_URL;
+
+  expect(
+    final_api ==
+      "https://finnhub.io/api/v1/search?&token=bv1uf4v48v6o5ed6h88g"
+  );
+});
+
+test("api_return_call_aapl_stock", () => {
+    const key = "bv1uf4v48v6o5ed6h88g";
+    const BASE_URL = "https://finnhub.io/api/v1/quote?symbol=";
+    const KEY_URL = '&token=${key}';
+    stock = 'AAPL'
+    const final_api = BASE_URL + stock + KEY_URL;
+  
+    expect(
+      final_api ==
+        "https://finnhub.io/api/v1/search?q=apple&token=bv1uf4v48v6o5ed6h88g"
+    );
+  });
+
+  test("api_return_call_spy_stock", () => {
+    const key = "bv1uf4v48v6o5ed6h88g";
+    const BASE_URL = "https://finnhub.io/api/v1/quote?symbol=";
+    const KEY_URL = '&token=${key}';
+    stock = 'SPY'
+    const final_api = BASE_URL + stock + KEY_URL;
+  
+    expect(
+      final_api ==
+        "https://finnhub.io/api/v1/search?q=apple&token=bv1uf4v48v6o5ed6h88g"
+    );
   });
