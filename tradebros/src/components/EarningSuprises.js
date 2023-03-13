858a70575d44e4071e684f7692cb7d79554feb58
skiislam <sisla016@ucr.edu>
2023-03-13 12:47:15 -0700

    Putting in old code

--- tradebros/src/components/EarningSuprises.js
+++ tradebros/src/components/EarningSuprises.js
@@ -7,45 +7,117 @@ export default function EarningSuprises(){
 
 
 const key = "bv1uf4v48v6o5ed6h88g";
-const EARNING_URL = "https://finnhub.io/api/v1/stock/earnings?symbol=TSLA";
+const TSLA_URL = "https://finnhub.io/api/v1/stock/earnings?symbol=TSLA";
+const AAPL_URL = "https://finnhub.io/api/v1/stock/earnings?symbol=TSLA";
+const MSFT_URL = "https://finnhub.io/api/v1/stock/earnings?symbol=TSLA";
+
 const KEY_URL = `&token=${key}`;
-const[earningSupriseData, setEarningSupriseData] = useState([]);
-const[updatedEarningSupriseData, setUpdatedEarningSupriseData] = useState([]);
+const[earningTSLASupriseData, setTSLAEarningSupriseData] = useState([]);
+const[earningAAPLSupriseData, setAAPLEarningSupriseData] = useState([]);
+const[earningMSFTSupriseData, setMSFTEarningSupriseData] = useState([]);
 
 useEffect(() => {
-    axios.get(`${EARNING_URL}${KEY_URL}`).then(res=> {
-        setEarningSupriseData(res.data)
+    axios.get(`${TSLA_URL}${KEY_URL}`).then(res=> {
+        setTSLAEarningSupriseData(res.data)
     })
     .catch(err=>{
         console.log(err)
     })
 }, [])
 useEffect(() => {
-    if (earningSupriseData.length > 4) {
-        earningSupriseData.splice(0, 4);
-    }
-    setUpdatedEarningSupriseData(earningSupriseData);
-  });
+    axios.get(`${AAPL_URL}${KEY_URL}`).then(res=> {
+        setAAPLEarningSupriseData(res.data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+}, [])
+useEffect(() => {
+    axios.get(`${MSFT_URL}${KEY_URL}`).then(res=> {
+        setMSFTEarningSupriseData(res.data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+}, [])
+
 
     return(
 <Fragment> 
-      <h1> Earning Report of TSLA </h1>
+      <h1> Earning Report </h1>
+      <h2 className="EPSHeading"> TSLA Earning Report</h2>
         <ul>
-          {earningSupriseData.map((stock) => {
+          {earningTSLASupriseData.map((stock) => {
         return(
           <ul>
               <table>
-                  <tr>
+                <thead>
+                <tr>
                       <th>Period</th>
                       <th>Actual</th>
                       <th>Estimate</th>
                   </tr>
+                </thead>
+                <tbody> 
                   <tr>
                       <td>{stock.period}</td>
                       <td>{stock.actual}</td>
                       <td>{stock.estimate}</td>                      
 
                   </tr>
+                </tbody>
+              </table>
+          </ul>
+      )
+  })}
+        </ul>
+        <h2 className="EPSHeading"> AAPL Earning Report</h2>
+        <ul>
+          {earningAAPLSupriseData.map((stock) => {
+        return(
+          <ul>
+              <table>
+                <thead>
+                <tr>
+                      <th>Period</th>
+                      <th>Actual</th>
+                      <th>Estimate</th>
+                  </tr>
+                </thead>
+                <tbody> 
+                  <tr>
+                      <td>{stock.period}</td>
+                      <td>{stock.actual}</td>
+                      <td>{stock.estimate}</td>                      
+
+                  </tr>
+                </tbody>
+              </table>
+          </ul>
+      )
+  })}
+    <h2 className="EPSHeading"> MSFT Earning Report</h2>
+        </ul>
+        <ul>
+          {earningMSFTSupriseData.map((stock) => {
+        return(
+          <ul>
+              <table>
+                <thead>
+                <tr>
+                      <th>Period</th>
+                      <th>Actual</th>
+                      <th>Estimate</th>
+                  </tr>
+                </thead>
+                <tbody> 
+                  <tr>
+                      <td>{stock.period}</td>
+                      <td>{stock.actual}</td>
+                      <td>{stock.estimate}</td>                      
+
+                  </tr>
+                </tbody>
               </table>
           </ul>
       )
