6732951dc2c6f1bc801cf7f5e04ad696597d010e
mallie2000 <muzammilallie230@gmail.com>
2023-03-13 15:15:06 -0700

    Added more snapshot test

--- tradebros/src/cases.test.js
+++ tradebros/src/cases.test.js
@@ -1,7 +1,21 @@
 import renderer from 'react-test-renderer';
 import Learn from './Pages/Learn.js';
+import Features from './Pages/Features.js';
+import Market from './Pages/Market.js';
+it('Market renders correctly', () => {
+  const tree = renderer
+    .create(<Market/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+it('Features renders correctly', () => {
+  const tree = renderer
+    .create(<Features/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-it('renders correctly', () => {
+it('Learn renders correctly', () => {
   const tree = renderer
     .create(<Learn/>)
     .toJSON();
