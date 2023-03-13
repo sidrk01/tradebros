d76741551613220357b37eea2b8ab8d2080369e2
Vinh Vu <VincentVu05@gmail.com>
2023-03-13 05:51:01 -0700

    Add buttons into features

---
+++ tradebros/src/Pages/Features.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Features.css";
+export default function Features() {
+  return (
+    
+      <div className="container-features">
+        <a href = "/News" target = "_blank">
+          <img src="https://www.freeiconspng.com/uploads/news-icon-24.png"></img></a>
+          <a href = "/Watchlists" target = "_blank">
+          <img src="https://cdn0.iconfinder.com/data/icons/disney-plus-disney-1/160/smartphone_watchlist-512.png"></img></a>
+          <a href = "/stock_performance" target = "_blank">
+          <img src="https://cdn-icons-png.flaticon.com/512/2737/2737450.png"></img></a>
+        </div>
+  );
+}
